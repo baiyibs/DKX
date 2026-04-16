3848f5fa82d3b574d5a10589d58373d59659a124
baiyi <baiyitongxue@qq.com>
2026-04-16 18:25:31 +0800

    ✨ feat: 使用户状态灯闪烁

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -95,7 +95,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+    HAL_GPIO_TogglePin(LED_STATUS_GPIO_Port, LED_STATUS_Pin);
+    HAL_Delay(500);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
